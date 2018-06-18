ff33b5f3f3580747c1d6ec91f3adb62278216b8b
Emerson Schulze <emerson7e@gmail.com>
2018-06-18 00:02:24 -0300

    Adicionar arquivos de projeto.

---
+++ SigfazCore/Areas/Basico/Models/Estado.cs
@@ -0,0 +1,18 @@
+﻿using SigfazCore.Models;
+using System.Collections.Generic;
+
+namespace SigfazCore.Areas.Basico.Models
+{
+    public class Estado : Entidade
+    {
+        public Estado()
+        {
+            Cidades = new HashSet<Cidade>();
+        }
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+        public int CodigoIbge { get; set; }
+        public virtual ICollection<Cidade> Cidades { get; set; }
+
+    }
+}
---
+++ SigfazCore/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using SigfazCore.Models;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
+
+namespace SigfazCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+                 services.AddDbContext<SigfazCoreContext>(options =>
+                options.UseMySql(Configuration.GetConnectionString("SigfazConect")));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("fiver",
+                    policy => policy.WithOrigins("http://localhost:4200"));
+            });
+
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("fiver"));
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                  name: "areas",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+        }
+    }
+}
